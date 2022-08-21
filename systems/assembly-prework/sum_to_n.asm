69932fb93d3172305185419a3dd3564ac2760f4e
Panashe M. Fundira <5841072+munyari@users.noreply.github.com>
2022-08-21 07:13:20 -0400

    Add assembly prework

--- systems/assembly-prework/sum_to_n.asm
+++ systems/assembly-prework/sum_to_n.asm
@@ -1,4 +1,10 @@
 section .text
 global sum_to_n
 sum_to_n:
+	mov rax, 0 ; empty the return register
+	mov rbx, rdi ; initialize a register to the value of the argument
+.loop:
+	add	rax, rbx ; add to running sum
+	dec	rbx ; decrement counter
+	jnp	.loop ; This relies on the fact that the parity flag is set to 0 if the decrement results in 0.
 	ret
